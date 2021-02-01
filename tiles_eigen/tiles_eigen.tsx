e4377e844d78bcd4cf37e5da6111cb140814cd2c
philipp-suess <77891443+philipp-suess@users.noreply.github.com>
2021-02-01 15:35:49 +0000

    Deploying to gh-pages from master @ 433e478686a49e6839ad2ce20f3238e4be6ac08c 🚀

--- tiles_eigen/tiles_eigen.tsx
+++ tiles_eigen/tiles_eigen.tsx
@@ -1,13 +1,40 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="tiles Eigen" tilewidth="34" tileheight="64" tilecount="3" columns="0">
+<tileset version="1.4" tiledversion="1.4.3" name="tiles Eigen" tilewidth="34" tileheight="64" tilecount="12" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="3">
-  <image width="34" height="34" source="Dogtag_test.png"/>
+  <image width="34" height="34" source="Dogtag_vorlage.png"/>
  </tile>
  <tile id="7">
   <image width="32" height="64" source="Vitrine.png"/>
  </tile>
  <tile id="8">
-  <image width="32" height="64" source="tilesets_deviant_milkian_2.png"/>
+  <image width="32" height="64" source="düse.png"/>
+ </tile>
+ <tile id="9">
+  <image width="32" height="64" source="ffp2.png"/>
+ </tile>
+ <tile id="10">
+  <image width="32" height="64" source="greifer.png"/>
+ </tile>
+ <tile id="11">
+  <image width="32" height="64" source="klammer.png"/>
+ </tile>
+ <tile id="12">
+  <image width="32" height="64" source="verdampfer.png"/>
+ </tile>
+ <tile id="13">
+  <image width="34" height="33" source="green.png"/>
+ </tile>
+ <tile id="14">
+  <image width="34" height="34" source="greengrey.png"/>
+ </tile>
+ <tile id="15">
+  <image width="34" height="34" source="grey.png"/>
+ </tile>
+ <tile id="16">
+  <image width="34" height="34" source="red.png"/>
+ </tile>
+ <tile id="17">
+  <image width="34" height="34" source="Teppich grau.png"/>
  </tile>
 </tileset>
