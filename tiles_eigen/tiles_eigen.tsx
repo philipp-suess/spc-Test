cef7aabe1433b2fd95880ad4540af93eb8eb6a11
philipp-suess <77891443+philipp-suess@users.noreply.github.com>
2021-02-01 16:40:54 +0000

    Deploying to gh-pages from master @ 4989444544a21ffab79e3d9798f6f7e414ad5d0e 🚀

--- tiles_eigen/tiles_eigen.tsx
+++ tiles_eigen/tiles_eigen.tsx
@@ -1,5 +1,8 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="tiles Eigen" tilewidth="128" tileheight="64" tilecount="13" columns="0">
+ <editorsettings>
+  <export target="tiles_eigen.png" format="json"/>
+ </editorsettings>
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="3">
   <image width="34" height="34" source="Dogtag_vorlage.png"/>
