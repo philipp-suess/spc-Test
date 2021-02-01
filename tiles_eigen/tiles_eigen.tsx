106375bee2038846fb61493d71a33043a0998146
philipp-suess <77891443+philipp-suess@users.noreply.github.com>
2021-02-01 15:36:48 +0100

    düse

---
+++ tiles_eigen/tiles_eigen.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="tiles Eigen" tilewidth="34" tileheight="64" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="3">
+  <image width="34" height="34" source="Dogtag_test.png"/>
+ </tile>
+ <tile id="7">
+  <image width="32" height="64" source="Vitrine.png"/>
+ </tile>
+ <tile id="8">
+  <image width="32" height="64" source="tilesets_deviant_milkian_2.png"/>
+ </tile>
+</tileset>
