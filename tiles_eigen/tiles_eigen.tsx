662a3f52da5b8b46eb6ea352332169aa627a5839
philipp-suess <77891443+philipp-suess@users.noreply.github.com>
2021-02-01 16:34:46 +0100

    teppich

--- tiles_eigen/tiles_eigen.tsx
+++ tiles_eigen/tiles_eigen.tsx
@@ -1,14 +1,14 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="tiles Eigen" tilewidth="34" tileheight="64" tilecount="7" columns="0">
+<tileset version="1.4" tiledversion="1.4.3" name="tiles Eigen" tilewidth="34" tileheight="64" tilecount="12" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="3">
-  <image width="34" height="34" source="Dogtag_test.png"/>
+  <image width="34" height="34" source="Dogtag_vorlage.png"/>
  </tile>
  <tile id="7">
   <image width="32" height="64" source="Vitrine.png"/>
  </tile>
  <tile id="8">
-  <image width="32" height="64" source="tilesets_deviant_milkian_2.png"/>
+  <image width="32" height="64" source="düse.png"/>
  </tile>
  <tile id="9">
   <image width="32" height="64" source="ffp2.png"/>
@@ -21,5 +21,20 @@
  </tile>
  <tile id="12">
   <image width="32" height="64" source="verdampfer.png"/>
+ </tile>
+ <tile id="13">
+  <image width="34" height="33" source="green.png"/>
+ </tile>
+ <tile id="14">
+  <image width="34" height="34" source="greengrey.png"/>
+ </tile>
+ <tile id="15">
+  <image width="34" height="34" source="grey.png"/>
+ </tile>
+ <tile id="16">
+  <image width="34" height="34" source="red.png"/>
+ </tile>
+ <tile id="17">
+  <image width="34" height="34" source="Teppich grau.png"/>
  </tile>
 </tileset>
