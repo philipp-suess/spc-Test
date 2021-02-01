aa8fdaa3a77bdfa9d7854ea205fe76026188b9d0
philipp-suess <77891443+philipp-suess@users.noreply.github.com>
2021-02-01 17:59:37 +0100

    tileset

--- tiles_eigen/tiles_eigen.tsx
+++ tiles_eigen/tiles_eigen.tsx
@@ -1,46 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="tiles Eigen" tilewidth="128" tileheight="64" tilecount="13" columns="0">
- <editorsettings>
-  <export target="tiles_eigen.png" format="json"/>
- </editorsettings>
+<tileset version="1.4" tiledversion="1.4.3" name="eigen" tilewidth="32" tileheight="32" tilecount="1" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="3">
-  <image width="34" height="34" source="Dogtag_vorlage.png"/>
- </tile>
- <tile id="7">
-  <image width="32" height="64" source="Vitrine.png"/>
- </tile>
- <tile id="8">
-  <image width="32" height="64" source="düse.png"/>
- </tile>
- <tile id="9">
-  <image width="32" height="64" source="ffp2.png"/>
- </tile>
- <tile id="10">
-  <image width="32" height="64" source="greifer.png"/>
- </tile>
- <tile id="11">
-  <image width="32" height="64" source="klammer.png"/>
- </tile>
- <tile id="12">
-  <image width="32" height="64" source="verdampfer.png"/>
- </tile>
- <tile id="25">
-  <image width="128" height="32" source="LOGO8.png"/>
- </tile>
- <tile id="26">
-  <image width="32" height="32" source="greengrey.png"/>
- </tile>
- <tile id="27">
+ <tile id="0">
   <image width="32" height="32" source="green.png"/>
- </tile>
- <tile id="28">
-  <image width="32" height="32" source="grey.png"/>
- </tile>
- <tile id="29">
-  <image width="32" height="32" source="red.png"/>
- </tile>
- <tile id="30">
-  <image width="32" height="32" source="Teppich grau.png"/>
  </tile>
 </tileset>
