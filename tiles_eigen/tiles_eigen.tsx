f13eb4f2205f407509f82217a513a942dc73e4ba
philipp-suess <77891443+philipp-suess@users.noreply.github.com>
2021-02-01 16:09:50 +0100

    ausstellung

--- tiles_eigen/tiles_eigen.tsx
+++ tiles_eigen/tiles_eigen.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="tiles Eigen" tilewidth="34" tileheight="64" tilecount="3" columns="0">
+<tileset version="1.4" tiledversion="1.4.3" name="tiles Eigen" tilewidth="34" tileheight="64" tilecount="7" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="3">
   <image width="34" height="34" source="Dogtag_test.png"/>
@@ -9,5 +9,17 @@
  </tile>
  <tile id="8">
   <image width="32" height="64" source="tilesets_deviant_milkian_2.png"/>
+ </tile>
+ <tile id="9">
+  <image width="32" height="64" source="ffp2.png"/>
+ </tile>
+ <tile id="10">
+  <image width="32" height="64" source="greifer.png"/>
+ </tile>
+ <tile id="11">
+  <image width="32" height="64" source="klammer.png"/>
+ </tile>
+ <tile id="12">
+  <image width="32" height="64" source="verdampfer.png"/>
  </tile>
 </tileset>
