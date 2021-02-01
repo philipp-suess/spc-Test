0e62f27b973499b398f54238534c61c2b2c5df94
philipp-suess <77891443+philipp-suess@users.noreply.github.com>
2021-02-01 17:49:15 +0100

    Update TilesetEigen.tsx

--- TilesetEigen.tsx
+++ TilesetEigen.tsx
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="TilesetEigen" tilewidth="32" tileheight="32" tilecount="1" columns="0">
+<tileset version="1.4" tiledversion="1.4.3" name="TilesetEigen" tilewidth="0" tileheight="0" tilecount="1" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
-  <image width="32" height="32" source="tiles_eigen/green.png"/>
+  <image source="https://philipp-suess.github.io/world/tiles_eigen/green.png"/>
  </tile>
 </tileset>
