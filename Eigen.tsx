4989444544a21ffab79e3d9798f6f7e414ad5d0e
philipp-suess <77891443+philipp-suess@users.noreply.github.com>
2021-02-01 17:40:12 +0100

    tiles entfehrnt

--- Eigen.tsx
+++
@@ -1,7 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="Eigen" tilewidth="216" tileheight="217" tilecount="1" columns="0">
- <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="0">
-  <image width="216" height="217" source="C:/Users/phili/Desktop/Tiles/Dogtag_Vorlage.png"/>
- </tile>
-</tileset>
