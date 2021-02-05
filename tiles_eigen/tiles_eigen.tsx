747bc5a3837623576fa7aee36a4644fa4da03544
philipp-suess <77891443+philipp-suess@users.noreply.github.com>
2021-02-05 15:52:00 +0000

    Deploying to gh-pages from master @ 8da702edc8ab8538593ff577f5f0ef31f7a2aad7 🚀

--- tiles_eigen/tiles_eigen.tsx
+++ tiles_eigen/tiles_eigen.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="eigen" tilewidth="128" tileheight="64" tilecount="17" columns="0">
+<tileset version="1.4" tiledversion="1.4.3" name="eigen" tilewidth="128" tileheight="64" tilecount="18" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="32" height="32" source="green.png"/>
@@ -54,5 +54,8 @@
  </tile>
  <tile id="18">
   <image width="32" height="64" source="schaltpult_kl.png"/>
+ </tile>
+ <tile id="19">
+  <image width="32" height="64" source="usb_kl.png"/>
  </tile>
 </tileset>
