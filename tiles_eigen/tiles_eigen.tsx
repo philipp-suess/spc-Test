7edd9e28326c947d087d938fe0d66438028cc17b
philipp-suess <77891443+philipp-suess@users.noreply.github.com>
2021-02-20 19:39:30 +0000

    Deploying to gh-pages from master @ 1b8e871de491a7f79ddda15b8f7d726990b023b6 🚀

--- tiles_eigen/tiles_eigen.tsx
+++ tiles_eigen/tiles_eigen.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="eigen" tilewidth="128" tileheight="64" tilecount="25" columns="0">
+<tileset version="1.4" tiledversion="1.4.3" name="eigen" tilewidth="128" tileheight="64" tilecount="26" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="32" height="32" source="green.png"/>
@@ -78,5 +78,8 @@
  </tile>
  <tile id="31">
   <image width="32" height="32" source="pfeil.png"/>
+ </tile>
+ <tile id="32">
+  <image width="32" height="64" source="ender.png"/>
  </tile>
 </tileset>
